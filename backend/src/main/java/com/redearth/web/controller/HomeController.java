58ea3b8d1e19c77f56f1ed414c3c2cf14196ccbc
noahrose82 <73205797+noahrose82@users.noreply.github.com>
2026-04-02 15:13:32 -0700

    Add home endpoint

--- backend/src/main/java/com/redearth/web/controller/HomeController.java
+++ backend/src/main/java/com/redearth/web/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-public class HomeControler {
+public class HomeController {
     
     @GetMapping("/")
     public String home() {
