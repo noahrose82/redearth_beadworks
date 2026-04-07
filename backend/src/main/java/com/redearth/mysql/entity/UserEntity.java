05222b4664fdf0e710c503018dc4a9e729da822b
noahrose82 <73205797+noahrose82@users.noreply.github.com>
2026-04-06 19:38:50 -0700

    Fix user created_at on register

--- backend/src/main/java/com/redearth/mysql/entity/UserEntity.java
+++ backend/src/main/java/com/redearth/mysql/entity/UserEntity.java
@@ -1,14 +1,7 @@
 package com.redearth.mysql.entity;
 
+import jakarta.persistence.*;
 import java.time.Instant;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
